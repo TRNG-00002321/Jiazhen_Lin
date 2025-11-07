1367120974b62c79e29f6044a3e03118570f608b
jzlin11 <01jzlin@gmail.com>
2025-11-07 15:18:44 -0600

    refactor

--- Week1/Day3/OOProject/greet.py
+++
@@ -1,7 +0,0 @@
-name = "oof"
-
-def say_hello():
-        return f"Hello {name}"
-
-def say_goodbye():
-        return f"Goodbye {name}"
--- Week1/Day3/OOProject/importdemo.py
+++
@@ -1,9 +0,0 @@
-from greet import say_hello,say_goodbye
-print(say_hello())
-print(say_goodbye())
-
-import Assignments.FunctionProblems
-print(Assignments.FunctionProblems.hello_function())
-
-import math
-print(dir(math))
